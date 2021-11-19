e842cd443d5a0dd05d26d2930dd56190387eb900
Sangha Lee <totoriato@gmail.com>
2021-11-20 04:04:31 +0900

    update

--- library/fetch.js
+++ library/fetch.js
@@ -2,6 +2,7 @@
 // @exclude     *
 // @grant       GM.xmlhttpRequest
 // ==UserLibrary==
+// @name        Promisified GM_xmlhttpRequest
 // @description 비동기(Promise) 웹 요청 라이브러리
 // @version     1.0.0
 // @author      toriato
